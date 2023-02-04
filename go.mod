27a8639dd2ab42974608260cb68ddb24627a168f
sheriff-kurdi <sheriff.kurdi@gmail.com>
2023-02-04 12:58:48 +0200

    refactor

--- go.mod
+++ go.mod
@@ -18,6 +18,7 @@ require (
 	github.com/jackc/pgx/v5 v5.2.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.0 // indirect
 	github.com/klauspost/compress v1.15.15 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
--- src/infrastructure/database/database.go
+++ src/infrastructure/database/database.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"os"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -10,8 +12,8 @@ var PostgresDB *gorm.DB
 var SqliteDB *gorm.DB
 
 func Connect() {
-	dsn := "host=10.5.0.2 user=postgres password=123456789 dbname=kurdi_go port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	database, err := gorm.Open(postgres.Open(os.Getenv("DB_CONNECTION")), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
